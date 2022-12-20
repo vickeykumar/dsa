7e433f61750204589a52ce49a568fb132c182a11
root <root@localhost.localdomain>
2022-12-20 05:06:28 -0500

    init

---
+++ include/search.h
@@ -0,0 +1,8 @@
+#ifndef __CDA_SEARCH_H
+#define __DSA_SEARCH_H
+
+int LinearSearch(int *arr, int size, int item);
+
+int BinarySearch(int *arr, int size, int item);
+
+#endif
---
+++ include/sort.h
@@ -0,0 +1,5 @@
+#ifndef __DSA_SORT_H
+#define __DSA_SORT_H
+
+
+#endif
