766ca918e799d54c8b07282393b0f150b1d17054
vickeykumar <kumarvickey45@gmail.com>
2022-12-22 10:49:18 +0000

    q and merge sort

--- include/sort.h
+++ include/sort.h
@@ -1,7 +1,26 @@
 #ifndef __DSA_SORT_H
 #define __DSA_SORT_H
+
 #include<stdio.h>
 
 extern void selection_sort(int *arr, int low, int high);
+/*
+ * sorts the array between index low and high
+ */
+void bubble_sort(int *arr, int low, int high);
+
+/*
+ * sorts the array between index low and high
+ */
+void insertion_sort(int *arr, int low, int high);
+
+/*
+ * sorts the array between index low and high
+ */
+void quick_sort(int *arr, int low, int high);
+void randomized_quick_sort(int *arr, int low, int high);
+
+/* merge 2 sorted array using in place sorting */
+void merge(int *arr, int low, int mid, int high);
 
 #endif
--- src/sort.c
+++ src/sort.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "../include/sort.h"
 
 void swap_int(int *arr, int i, int j) {
@@ -33,6 +34,7 @@ void bubble_sort(int *arr, int low, int high) {
 
 /*
  * sorts the array between index low and high
+ * O(n2) , best case O(n)
  */
 void insertion_sort(int *arr, int low, int high) {
     for (int i=low; i<high; i++) {
@@ -43,5 +45,69 @@ void insertion_sort(int *arr, int low, int high) {
              j--;
         }
         arr[j+1]=item;
+    }
+}
+
+/* returns index of the element after partition,
+ * such that all the elements left are less then  and all the elements right
+ * are greater then the partitioned element 
+ */
+int partition(int *arr, int low, int high) {
+    int ipivot = low;
+    int pivot = arr[ipivot];
+    for(int i=ipivot+1; i<=high; i++) {
+        if (arr[i]<=pivot) {
+            ipivot++;
+            swap_int(arr, i, ipivot);
+        }
+    }
+    swap_int(arr, ipivot,low);
+    return ipivot;
+}
+
+/*
+ * sorts the array between index low and high
+ */
+void quick_sort(int *arr, int low, int high) {
+    if(low<high) {
+        int p = partition(arr, low, high);
+        quick_sort(arr, low, p-1);
+        quick_sort(arr, p+1, high);
+    }
+}
+
+void randomized_quick_sort(int *arr, int low, int high) {
+    if(low<high) {
+        int i=rand()%(high-low+1);
+        swap_int(arr, i, low);
+        int p = partition(arr, low, high);
+        quick_sort(arr, low, p-1);
+        quick_sort(arr, p+1, high);
+    }
+}
+
+/* merge 2 sorted array using in place sorting */
+void merge(int *arr, int low, int mid, int high) {
+    for (int i=mid; i<=high; i++) {
+        int item = arr[i];
+        int j=i-1;
+        while(j>=low && arr[j]>item) { 
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=item;
+        low = j+1;  // with each iteration this low par will keep increasing, as minimum of second array will always be greater than the next j
+    }
+}
+
+/*
+ * sorts the array between index low and high
+ */
+void merge_sort(int *arr, int low, int high) {
+    if(low<high) {
+        int mid = (low+high)/2;
+        merge_sort(arr, low, mid);
+        merge_sort(arr, mid+1, high);
+        merge(arr, low, mid, high);
     }
 }
